c4bb28a0727da25a70182fa164f6c82f4f001890
mhega <m.mohie83@hotmail.com>
2022-04-22 12:57:41 -0400

    Update Lookup.py

--- Lookup.py
+++ Lookup.py
@@ -1,9 +1,10 @@
+from ListTables import printList
+
 class Lookup:
     # For allowing storing and looking up variables using variable sets of key pairs.
     # Each key pair is a key-value pair tuple.
     # The entire set of key pair tuples uniquely stores and retreives a variable value.
     # frozenset is used to achieve the benifit of a set while ensuring immutability, so it can be hashed for use as a dictionary key.
-    from ListTables import printList
     lookupDictionary = {}
     def put(*, keyPairs, value):
         Lookup.lookupDictionary[frozenset(keyPairs)] = value
@@ -20,7 +21,6 @@ class Lookup:
         return [Lookup.lookupDictionary[i] for i in set(Lookup.lookupDictionary.keys()) if set(keyPairs).issubset(i)]
     def printAllItems(*, keyPairs):
         printList(('',''), [(set(i[0]), i[1]) for i in list(Lookup.lookupDictionary.items()) if set(keyPairs).issubset(set(i[0]))]) 
-
 class utils:
     def setCacheEntry(*, zipFileName, functionName, value):
         Lookup.put(keyPairs = [('__zipFileName__',zipFileName),('__functionName__',functionName)],value=value)
